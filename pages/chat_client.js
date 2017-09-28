828890b173f7f45387b921dca82566d468920e3f
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-09-28 18:30:15 +0200

    various improvements (one of them is mysql support).

--- pages/chat_client.js
+++ pages/chat_client.js
@@ -1,4 +1,4 @@
-var base_dir = window.location.pathname.split("/").slice(0,-1).join("/");
+var base_dir = window.location.pathname.split('/').slice(0,-1).join('/');
 
 // jQuery plugin to prevent double submission of forms
 jQuery.fn.preventDoubleSubmission = function() {
@@ -24,7 +24,7 @@ function isScrolledToBottom(){ return $('html')[0].scrollTop == scrollHeight() }
 function scrollToBottom(){ $('html')[0].scrollTop = scrollHeight() }
 
 // message listing
-function listMessages(scrollFlag){ $("#message_log").load(base_dir+"/?location=chat/list", function(){ if(scrollFlag) scrollToBottom() } ) }
+function listMessages(scrollFlag){ $('#message_log').load(base_dir+'/?location=chat/list', function(){ if(scrollFlag) scrollToBottom() } ) }
 function periodicallyListMessagesCallback(){ var scrollFlag = isScrolledToBottom(); listMessages(scrollFlag) }
 
 // on ready
@@ -34,25 +34,25 @@ $(function(){
 	setInterval(periodicallyListMessagesCallback, 1000) // get messages periodically
 
 	// allow message sending
-	$("form#send_message_form").preventDoubleSubmission();
-	$("form#send_message_form").submit(function(e){
+	$('form#send_message_form').preventDoubleSubmission();
+	$('form#send_message_form').submit(function(e){
 		e.preventDefault() // prevent page reload
 
-		var input = $("input[name=message_text]");
-		if(input.val()=="") return false // no blank message allowed
-		var serialized_form = $("form#send_message_form").serialize();
-		input.prop("disabled", true) // disable input on submit
+		var input = $('input[name=message_text]');
+		if(input.val()=='') return false // no blank message allowed
+		var serialized_form = $('form#send_message_form').serialize();
+		input.prop('disabled', true) // disable input on submit
 
 		// send
-		$.post(base_dir+"/?location=chat/send", serialized_form) // send message
+		$.post(base_dir+'/?location=chat/send', serialized_form) // send message
 		.done(function(){
-			input.val("")
+			input.val('')
 		})
 		.fail(function(){ // error handling
-			alert("network error, retry...")
+			alert('Error! (when sending a message)')
 		})
 		.always(function(){
-			input.prop("disabled", false) // restore input after submit
+			input.prop('disabled', false) // restore input after submit
 			input.focus()
 		})
 		return false
--- pages/index.php
+++ pages/index.php
@@ -1,86 +1,100 @@
 <?php
 
-$location=@$_REQUEST['location'];
+// supported DB types: 'sqlite', 'postgres', 'mysql'
+define('pdo_db_type', 'mysql');
 
-function pdo_sqlite(){
-	$db_url="sqlite:../db.sqlite";
-	return new PDO($db_url, "", "", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
-}
-
-function pdo_postgres(){
-	$db_url="pgsql:host=localhost;dbname=lines";
-	return new PDO($db_url, "postgres", "postgres", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
-}
-
-function db_type(){
-	$db_type="sqlite"; // DB types supported: sqlite, postgres
-	return $db_type;
-}
 function pdo(){
-	if(db_type()=="sqlite")
-		return pdo_sqlite();
-	else if(db_type()=="postgres")
-		return pdo_postgres();
+
+	switch(pdo_db_type){
+
+		case 'sqlite':
+			$db_url = 'sqlite:../db.sqlite';
+			$pdo = new PDO($db_url, "", "", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+			break;
+
+		case 'postgres':
+			$db_url = 'pgsql:host=localhost; dbname=messaging';
+			$pdo = new PDO($db_url, 'postgres', 'password', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+			break;
+
+		case 'mysql':
+			$db_url = 'mysql:host=localhost; dbname=messaging';
+			$pdo = new PDO($db_url, 'root', 'password', [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+			break;
+	}
+
+	return $pdo;
 }
 
-$location = explode("/", $location);
-if($location==['']){
-	
-	header("Location: chat_client.html");
-
-}elseif($location==['util','phpinfo']){
-
-	phpinfo();
-	
-}elseif($location[0]=='chat'){
-	$location=$location[1];
-	if($location=='setup'){
-
-    $pdo = pdo();
-
-	if(db_type()=="sqlite")
-    $sql = "CREATE TABLE IF NOT EXISTS chat_messages (
+function pdo_setup_db_sql(){
+	$setup_db_sql = [
+	'sqlite' => 'CREATE TABLE IF NOT EXISTS chat_messages (
       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
       message_text TEXT NOT NULL,
       sender TEXT NOT NULL,
-      creation_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL )";
+      creation_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL )',
 
-	else if(db_type()=="postgres")
-    $sql = "CREATE TABLE IF NOT EXISTS chat_messages (
+	'postgres' => 'CREATE TABLE IF NOT EXISTS chat_messages (
       id SERIAL PRIMARY KEY NOT NULL,
       message_text TEXT NOT NULL,
       sender TEXT NOT NULL,
-      creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL )";
-    
-    $pdo->query($sql); // init db tables
-    
-    echo "chat_messages table is setup";
-    
-	}elseif($location=='list'){
+      creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL )',
 
+    'mysql' => 'CREATE TABLE IF NOT EXISTS chat_messages (
+      id SERIAL PRIMARY KEY NOT NULL,
+      message_text TEXT NOT NULL,
+      sender TEXT NOT NULL,
+      creation_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL )',
+
+    ];
+	$sql = $setup_db_sql[pdo_db_type];
+	return $sql;
+}
+
+$location = @$_REQUEST['location'];
+
+// http://localhost:88
+if($location == ''){
+
+	header("Location: chat_client.html");
+
+// http://localhost:88/?location=chat/list
+} elseif($location=='chat/list') {
+
+	// list messages
 	$pdo = pdo();
-    
-    $query = "SELECT * FROM (SELECT * FROM chat_messages ORDER BY creation_timestamp DESC LIMIT 15) AS res ORDER BY creation_timestamp ASC";
-    $messages = $pdo->query($query); // list messages
+    $query = 'SELECT * FROM (SELECT * FROM chat_messages ORDER BY creation_timestamp DESC LIMIT 15) AS res ORDER BY creation_timestamp ASC';
+    $messages = $pdo->query($query);
 
+	// produce HTML output
     $output = '';
-
-    foreach($messages as $message){
-        $sender = htmlspecialchars($message["sender"]);
-        $text = htmlspecialchars($message["message_text"]);
-        $output .= $sender.": ".$text."<br>";
+    foreach($messages as $message) {
+        $sender = htmlspecialchars($message['sender']);
+        $text = htmlspecialchars($message['message_text']);
+        $output .= "$sender: $text<br>";
     }
-    
     echo $output;
-    
-	}elseif($location=='send'){
+
+// http://localhost:88/?location=chat/send&message_text=text1&sender=sender1
+} elseif($location == 'chat/send') {
+
+	// insert new message
+    $pdo = pdo();
+    $sql = 'INSERT INTO chat_messages (message_text, sender) VALUES (:text, :sender)';
+    $stat = $pdo->prepare($sql);
+    $stat->execute(['text' => @$_REQUEST['message_text'], 'sender' => @$_REQUEST['sender']]);
+
+// http://localhost:88/?location=util/db_setup
+} elseif($location == 'util/db_setup') {
 
     $pdo = pdo();
-    
-    $text = $pdo->quote($_REQUEST["message_text"]);
-    $sender = $pdo->quote($_REQUEST["sender"]);    
-    $sql = "INSERT INTO chat_messages (message_text, sender) VALUES ($text, $sender)";
-    $pdo->query($sql); // insert new message
-    
-	}
+	$sql = pdo_setup_db_sql(); // setup database tables
+    $pdo->query($sql);
+    echo 'DB tables are now setup.';
+
+// http://localhost:88/?location=util/phpinfo
+} elseif($location == 'util/phpinfo'){
+
+	phpinfo();
+
 }
